2f0c7e4c0bf241bb88d05e0ba958ebb41fcfc30a
radup99 <61940587+radup99@users.noreply.github.com>
2020-07-07 18:43:07 +0300

    removed unnecessary show function

--- file_list.py
+++ file_list.py
@@ -17,19 +17,3 @@ def get_files_folders(path_obj):
         files.remove(f)
 
     return files, folders
-
-
-def show(path_obj):
-
-    files, folders = get_files_folders(path_obj)
-    i = 0
-
-    print(str(path_obj), '\n')
-
-    for dir in folders:
-        i += 1
-        print(f"{i}. >{dir}")
-
-    for f in files:
-        i += 1
-        print(f"{i}. {f}")
